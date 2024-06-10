37b8a96b8addbef71cea0492da20aa7067de564d
Maurício Dias de Carvalho Machado <mauricio.machado@antt.gov.br>
2024-06-10 20:14:35 -0300

    Implementação da Controller que vai fazer o controle de Login do Usuário

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+using cadastra_endereco.Data;
+
+public class AccountController : Controller
+{
+    private ApplicationDbContext _context;
+
+    public AccountController()
+    {
+        _context = new ApplicationDbContext();
+    }
+
+    [HttpGet]
+    public ActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Login(string email, string senha)
+    {
+        var usuario = _context.Usuarios.FirstOrDefault(u => u.Email == email && u.Senha == senha);
+        if (usuario != null)
+        {
+            FormsAuthentication.SetAuthCookie(email, false);
+            return RedirectToAction("Index", "Endereco");
+        }
+        ModelState.AddModelError("", "Credenciais inválidas");
+        return View();
+    }
+
+    [HttpGet]
+    public ActionResult Logout()
+    {
+        FormsAuthentication.SignOut();
+        return RedirectToAction("Login");
+    }
+}
