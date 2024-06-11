09225e29f8e58be711c7823f4c0ca8499cf825e0
Maurício Dias de Carvalho Machado <mauricio.machado@antt.gov.br>
2024-06-11 18:33:54 -0300

    Implementação da Controller para cadastro de Endereços

--- Controllers/EnderecoController.cs
+++ Controllers/EnderecoController.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Web;
 using cadastra_endereco.Data;
 using cadastra_endereco.Models;
+using System;
 
 public class EnderecoController : Controller
 {
@@ -19,23 +20,31 @@ public class EnderecoController : Controller
 
     public ActionResult Index()
     {
-        // Verifica se a sessão do usuário está ativa
-        if (Session["UserEmail"] == null)
+        try
         {
-            // Redireciona para a tela de login se o usuário não estiver logado
-            return RedirectToAction("Login", "Account");
+            // Verifica se a sessão do usuário está ativa
+            if (Session["UserEmail"] == null)
+            {
+                // Redireciona para a tela de login se o usuário não estiver logado
+                return RedirectToAction("Login", "Account");
+            }
+
+            var usuarioEmail = Session["UserEmail"].ToString();
+            var usuario = _context.Usuarios.FirstOrDefault(u => u.Email == usuarioEmail);
+
+            if (usuario == null)
+            {
+                return HttpNotFound("Usuário não encontrado.");
+            }
+
+            var enderecos = _context.Enderecos.Where(e => e.UsuarioId == usuario.Id).ToList();
+            return View(enderecos);
         }
-
-        var usuarioEmail = Session["UserEmail"].ToString();
-        var usuario = _context.Usuarios.FirstOrDefault(u => u.Email == usuarioEmail);
-
-        if (usuario == null)
+        catch (Exception ex)
         {
-            return HttpNotFound("Usuário não encontrado.");
+            ViewBag.ErrorMessage = "Ocorreu um erro ao carregar os endereços.";
+            return View("Error");
         }
-
-        var enderecos = _context.Enderecos.Where(e => e.UsuarioId == usuario.Id).ToList();
-        return View(enderecos);
     }
 
 
@@ -48,30 +57,38 @@ public class EnderecoController : Controller
     [HttpPost]
     public ActionResult Create(Endereco endereco)
     {
-        // Verifica se a sessão do usuário está ativa
-        if (Session["UserEmail"] == null)
+        try
         {
-            // Redireciona para a tela de login se o usuário não estiver logado
-            return RedirectToAction("Login", "Account");
+            // Verifica se a sessão do usuário está ativa
+            if (Session["UserEmail"] == null)
+            {
+                // Redireciona para a tela de login se o usuário não estiver logado
+                return RedirectToAction("Login", "Account");
+            }
+
+            // Obtém o email do usuário da sessão
+            var usuarioEmail = Session["UserEmail"].ToString();
+            var usuario = _context.Usuarios.FirstOrDefault(u => u.Email == usuarioEmail);
+
+            if (usuario == null)
+            {
+                return HttpNotFound("Usuário não encontrado.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                endereco.UsuarioId = usuario.Id;
+                _context.Enderecos.Add(endereco);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(endereco);
         }
-
-        // Obtém o email do usuário da sessão
-        var usuarioEmail = Session["UserEmail"].ToString();
-        var usuario = _context.Usuarios.FirstOrDefault(u => u.Email == usuarioEmail);
-
-        if (usuario == null)
+        catch(Exception ex)
         {
-            return HttpNotFound("Usuário não encontrado.");
+            ViewBag.ErrorMessage = "Ocorreu um erro ao registrar um endereço.";
+            return View("Error");
         }
-
-        if (ModelState.IsValid)
-        {
-            endereco.UsuarioId = usuario.Id;
-            _context.Enderecos.Add(endereco);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-        return View(endereco);
     }
 
     [HttpGet]
@@ -89,13 +106,21 @@ public class EnderecoController : Controller
     [ValidateAntiForgeryToken]
     public ActionResult DeleteConfirmed(int id)
     {
-        var endereco = _context.Enderecos.Find(id);
-        if (endereco != null)
+        try
         {
-            _context.Enderecos.Remove(endereco);
-            _context.SaveChanges();
+            var endereco = _context.Enderecos.Find(id);
+            if (endereco != null)
+            {
+                _context.Enderecos.Remove(endereco);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
-        return RedirectToAction("Index");
+        catch(Exception ex)
+        {
+            ViewBag.ErrorMessage = "Ocorreu um erro ao deletar um endereço.";
+            return View("Error");
+        }
     }
 
     [HttpGet]
@@ -113,71 +138,95 @@ public class EnderecoController : Controller
     [ValidateAntiForgeryToken]
     public ActionResult Edit(Endereco endereco)
     {
-        if (ModelState.IsValid)
+        try
         {
-            var existingEndereco = _context.Enderecos.AsNoTracking().FirstOrDefault(e => e.Id == endereco.Id);
-            if (existingEndereco == null)
+            if (ModelState.IsValid)
             {
-                return HttpNotFound("Endereço não encontrado.");
+                var existingEndereco = _context.Enderecos.AsNoTracking().FirstOrDefault(e => e.Id == endereco.Id);
+                if (existingEndereco == null)
+                {
+                    return HttpNotFound("Endereço não encontrado.");
+                }
+
+                endereco.UsuarioId = existingEndereco.UsuarioId; // Preserve o UsuarioId existente
+
+                _context.Entry(endereco).State = System.Data.Entity.EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
             }
-
-            endereco.UsuarioId = existingEndereco.UsuarioId; // Preserve o UsuarioId existente
-
-            _context.Entry(endereco).State = System.Data.Entity.EntityState.Modified;
-            _context.SaveChanges();
-            return RedirectToAction("Index");
+            return View(endereco);
         }
-        return View(endereco);
+        catch(Exception ex)
+        {
+            ViewBag.ErrorMessage = "Ocorreu um erro ao Editar um endereço.";
+            return View("Error");
+        }
     }
 
 
     [HttpGet]
     public async Task<ActionResult> GetEnderecoPorCep(string cep)
     {
-        using (var client = new HttpClient())
+        try
         {
-            var response = await client.GetAsync($"https://viacep.com.br/ws/{cep}/json/");
-            if (response.IsSuccessStatusCode)
+            using (var client = new HttpClient())
             {
-                var jsonResult = await response.Content.ReadAsStringAsync();
-                var endereco = JsonConvert.DeserializeObject<dynamic>(jsonResult);
-                return Json(endereco, JsonRequestBehavior.AllowGet);
+                var response = await client.GetAsync($"https://viacep.com.br/ws/{cep}/json/");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResult = await response.Content.ReadAsStringAsync();
+                    var endereco = JsonConvert.DeserializeObject<dynamic>(jsonResult);
+                    return Json(endereco, JsonRequestBehavior.AllowGet);
+                }
             }
+            return Json(null, JsonRequestBehavior.AllowGet);
         }
-        return Json(null, JsonRequestBehavior.AllowGet);
+        catch(Exception ex)
+        {
+            ViewBag.ErrorMessage = "Ocorreu um erro ao tentar buscar o Endereço pelo CEP.";
+            return View("Error");
+        }
     }
 
 
     [HttpGet]
     public ActionResult ExportCsv()
     {
-        // Verifica se a sessão do usuário está ativa
-        if (Session["UserEmail"] == null)
+        try
         {
-            // Redireciona para a tela de login se o usuário não estiver logado
-            return RedirectToAction("Login", "Account");
+            // Verifica se a sessão do usuário está ativa
+            if (Session["UserEmail"] == null)
+            {
+                // Redireciona para a tela de login se o usuário não estiver logado
+                return RedirectToAction("Login", "Account");
+            }
+
+            // Obtém o email do usuário da sessão
+            var usuarioEmail = Session["UserEmail"].ToString();
+            var usuario = _context.Usuarios.FirstOrDefault(u => u.Email == usuarioEmail);
+
+            if (usuario == null)
+            {
+                return HttpNotFound("Usuário não encontrado.");
+            }
+
+            var enderecos = _context.Enderecos.Where(e => e.UsuarioId == usuario.Id).ToList();
+            var csv = new StringBuilder();
+            csv.AppendLine("Cep,Logradouro,Bairro,Cidade,Estado");
+
+            foreach (var endereco in enderecos)
+            {
+                csv.AppendLine($"{endereco.Cep},{endereco.Logradouro},{endereco.Bairro},{endereco.Cidade},{endereco.Estado}");
+            }
+
+            var byteArray = Encoding.UTF8.GetBytes(csv.ToString());
+            var stream = new System.IO.MemoryStream(byteArray);
+            return File(stream, "text/csv", "lista_de_enderecos.csv");
         }
-
-        // Obtém o email do usuário da sessão
-        var usuarioEmail = Session["UserEmail"].ToString();
-        var usuario = _context.Usuarios.FirstOrDefault(u => u.Email == usuarioEmail);
-
-        if (usuario == null)
+        catch(Exception ex)
         {
-            return HttpNotFound("Usuário não encontrado.");
+            ViewBag.ErrorMessage = "Ocorreu um erro ao tentar gerar o arquivo CSV.";
+            return View("Error");
         }
-
-        var enderecos = _context.Enderecos.Where(e => e.UsuarioId == usuario.Id).ToList();
-        var csv = new StringBuilder();
-        csv.AppendLine("Cep,Logradouro,Bairro,Cidade,Estado");
-
-        foreach (var endereco in enderecos)
-        {
-            csv.AppendLine($"{endereco.Cep},{endereco.Logradouro},{endereco.Bairro},{endereco.Cidade},{endereco.Estado}");
-        }
-
-        var byteArray = Encoding.UTF8.GetBytes(csv.ToString());
-        var stream = new System.IO.MemoryStream(byteArray);
-        return File(stream, "text/csv", "enderecos.csv");
     }
 }
